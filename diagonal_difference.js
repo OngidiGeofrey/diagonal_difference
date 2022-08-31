991e18bf72a2756d1f543f03607c2a2a3c1f3c24
Ongidi Geofrey <ongidigeofrey@gmail.com>
2022-08-31 07:56:34 -0400

    function diagonal_difference

--- diagonal_difference.js
+++ diagonal_difference.js
@@ -1,36 +1,32 @@
-
-
-// Javascript Code to Find difference between sums
-// of two diagonals
-
-function diagonal_difference(array) {
-    var n = array.length; 
-    var d1 = 0;
-    var d2 = 0;
-  for(var i=0; i<n; i++){
-     for(var j=0; j<n; j++){
-       // finding the sum of primary diagonal
-         if(i === j) {
-           d1 += Array[i][j];
-         }
-       // finding the sum of secondary diagonal
-         if(i + j === n - 1){
-            d2 += array[i][j];
-         }
-      }
-  }
-  return Math.abs(d1 - d2);
+function absoluteDifference(arr){
+    
+    var sumDiagnoalOne=0
+    var sumDiagnoalTwo=0
+    for(var i=0; i<arr.length; i++){
+    
+        for(var j=i; j<arr.length; j++){
+            sumDiagnoalOne+=arr[i][j]
+            break
+        }
+    }
+    var checkArray=[]
+    arr.map(array=>checkArray.push(array.reverse()))
+    for(var i=0; i<checkArray.length; i++){
+    
+        for(var j=i; j<checkArray.length; j++){
+            sumDiagnoalTwo+=checkArray[i][j]
+            break
+        }
+    }
+    document.write( Math.abs(sumDiagnoalOne- sumDiagnoalTwo))
 }
+
 
 /* Driver program to test above function */
      
-let n = 3;
-         
-let array =
-[
-    [1, 2, 3],
+let n = 3;       
+let arr = [[11, 2, 4],
     [4 , 5, 6],
-    [9, 8, 9]
-];
+    [10, 8, -12]];
 
-document.write(difference(array, n));
+absoluteDifference(arr);
