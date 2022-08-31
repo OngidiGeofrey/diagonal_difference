87fe704be019d643be6f3477a2a617f06f199911
Ongidi Geofrey <ongidigeofrey@gmail.com>
2022-08-31 08:16:42 -0400

    calculate the difference between sum of diagnols

--- diagonal_difference.js
+++ diagonal_difference.js
@@ -1,16 +1,27 @@
-function absoluteDifference(arr){
+// JAVA SCRIPT  Code to Find absolute difference between sums of two diagonals
+
+
+function diagonal_difference(array){
+
+    // Initialize sums of diagonals
     
     var sumDiagnoalOne=0
     var sumDiagnoalTwo=0
-    for(var i=0; i<arr.length; i++){
+
+
+    // finding sum of primary diagonal
+    for(var i=0; i<array.length; i++){
     
-        for(var j=i; j<arr.length; j++){
-            sumDiagnoalOne+=arr[i][j]
+        for(var j=i; j<array.length; j++){
+            sumDiagnoalOne+=array[i][j]
             break
         }
     }
     var checkArray=[]
     arr.map(array=>checkArray.push(array.reverse()))
+
+
+    // finding sum of secondary diagonal
     for(var i=0; i<checkArray.length; i++){
     
         for(var j=i; j<checkArray.length; j++){
@@ -18,15 +29,17 @@ function absoluteDifference(arr){
             break
         }
     }
-    document.write( Math.abs(sumDiagnoalOne- sumDiagnoalTwo))
+
+    //Absolute difference of the sums
+        // across the diagonals
+    return Math.abs(sumDiagnoalOne- sumDiagnoalTwo)
 }
 
 
 /* Driver program to test above function */
-     
-let n = 3;       
-let arr = [[11, 2, 4],
+          
+let arr = [[1, 2, 3],
     [4 , 5, 6],
-    [10, 8, -12]];
+    [9, 8, 9]];
 
-absoluteDifference(arr);
+console.log(diagonal_difference(arr));
